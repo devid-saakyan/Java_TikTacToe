6d1a6dda3860fa3d43bce45b51c5b5267f4773bb
devid-saakyan <59139074+devid-saakyan@users.noreply.github.com>
2020-10-30 23:04:08 +0300

    Update Tictactoe.java

--- Tictactoe.java
+++ Tictactoe.java
@@ -4,18 +4,13 @@ public class Tictactoe {
     private static final int COLUMNS = 3;
     private String regex = "\\s{3}";
 
-    /**
-     * Tictactoe constructor
-     */
+    
     public Tictactoe() {
         board = new String[ROWS][COLUMNS];
         this.initializeBoard();
-//		this.winXtest();
+
     }
 
-    /**
-     * initializing board and fill with empty spaces
-     */
     public void initializeBoard() {
         for(int i = 0; i < ROWS; i++) {
             for(int j = 0; j < COLUMNS; j++) {
@@ -24,20 +19,13 @@ public class Tictactoe {
         }
     }
 
-    /**
-     * setPlay method will set player's move
-     * @param i
-     * @param j
-     * @param player
-     */
+ 
     public void setPlay(int i, int j, String player) {
         if(board[i][j].matches(regex))
             board[i][j] = " "+player+" ";
     }
 
-    /**
-     * finding winners
-     */
+   
     public boolean isGameOver() {
         //checking rows
         for(int i = 0; i < ROWS; i++) {
@@ -45,13 +33,13 @@ public class Tictactoe {
                 return true;
             }
         }
-        //checking columns
+
 
         for(int j = 0; j < COLUMNS; j++) {
             if(!board[0][j].matches(regex) && board[0][j].equals(board[1][j]) && board[1][j].equals(board[2][j]))
                 return true;
         }
-        //checking diagonals
+
         if(!board[0][0].matches(regex) && board[0][0].equals(board[1][1]) && board[1][1].equals(board[2][2]))
             return true;
         if(!board[0][2].matches(regex) && board[0][2].equals(board[1][1]) && board[1][1].equals(board[2][0]))
@@ -61,10 +49,7 @@ public class Tictactoe {
 
     }
 
-    /**
-     * Print board to screen
-     * @return strBoard
-     */
+  
     public String printBoard() {
         String strBoard = "";
         for(int i = 0; i < ROWS; i++) {
@@ -80,9 +65,7 @@ public class Tictactoe {
         return strBoard;
     }
 
-    /**
-     * test unit 1st column X
-     */
+    
     public void winXtest() {
         for(int i = 0; i < ROWS; i++) {
             for(int j = 0; j < COLUMNS; j++) {
